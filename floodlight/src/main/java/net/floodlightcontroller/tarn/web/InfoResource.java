7aca0846835caf6f37a7ab66b1f920de15dd7a93
Geddings Barrineau <cbarrin@g.clemson.edu>
2017-07-30 02:08:16 -0700

    Added a basic info REST API and put it in the wiki

--- floodlight/src/main/java/net/floodlightcontroller/tarn/web/InfoResource.java
+++ floodlight/src/main/java/net/floodlightcontroller/tarn/web/InfoResource.java
@@ -1,12 +1,12 @@
 package net.floodlightcontroller.tarn.web;
 
 import org.restlet.resource.Get;
-import org.restlet.routing.Route;
-import org.restlet.routing.Router;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import com.google.common.collect.ImmutableMap;
+
+import net.floodlightcontroller.tarn.IRandomizerService;
 
 /**
  * Created by geddingsbarrineau on 10/29/16.
@@ -15,31 +15,11 @@ import java.util.List;
 public class InfoResource extends ServerResource {
     protected static Logger log = LoggerFactory.getLogger(InfoResource.class);
 
-//    public static class InfoJsonSerializerWrapper {
-//        private final String prefix;
-//        private final List<RandomizedHost> randomizedHosts;
-//
-//        public InfoJsonSerializerWrapper(String prefix, List<RandomizedHost> randomizedHosts) {
-//            this.prefix = prefix;
-//            this.randomizedHosts = randomizedHosts;
-//        }
-//    }
-
-//    @Get
-//    public Object getEAGERInfo() {
-//        IRandomizerService randomizerService = (IRandomizerService) getContext().getAttributes().get(IRandomizerService.class.getCanonicalName());
-//        Map<String, String> ret = new HashMap<>();
-//        // FIXME: This is broken with the new prefixes implementation
-//        ret.put("current-prefix", randomizerService.getCurrentPrefix().toString());
-//        return ret;
-//    }
-
     @Get
-    public Object getAllRoutes() {
-        Router router = (Router)this.getApplication().getInboundRoot();
-        for (Route route : router.getRoutes()) {
-            log.info("{}", route);
-        }
-        return router.getRoutes();
+    public ImmutableMap<String, Object> getTARNInfo() {
+        IRandomizerService tarn = (IRandomizerService) getContext().getAttributes().get(IRandomizerService.class.getCanonicalName());
+        ImmutableMap.Builder<String, Object> info = new ImmutableMap.Builder<>();
+        info.put("as-count", tarn.getAutonomousSystems().size());
+        return info.build();
     }
 }

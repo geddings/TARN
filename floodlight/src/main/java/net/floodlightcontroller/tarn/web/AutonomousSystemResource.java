677c6985f2d2092c9555582f7449a104eca6da8a
Geddings Barrineau <cbarrin@g.clemson.edu>
2017-07-31 22:41:47 -0700

    Added ability to dynamically add a prefix to an AS's prefix pool through the REST API.

--- floodlight/src/main/java/net/floodlightcontroller/tarn/web/AutonomousSystemResource.java
+++ floodlight/src/main/java/net/floodlightcontroller/tarn/web/AutonomousSystemResource.java
@@ -1,9 +1,16 @@
 package net.floodlightcontroller.tarn.web;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Optional;
 
+import org.projectfloodlight.openflow.types.IPv4AddressWithMask;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
+import org.restlet.resource.Put;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.floodlightcontroller.tarn.AutonomousSystem;
 import net.floodlightcontroller.tarn.IRandomizerService;
@@ -16,8 +23,37 @@ public class AutonomousSystemResource extends ServerResource {
     @Get
     public Object getAutonomousSystem() {
         IRandomizerService randomizerService = (IRandomizerService) getContext().getAttributes().get(IRandomizerService.class.getCanonicalName());
+
         Integer asNumber = Integer.valueOf(getRequestAttributes().get("as-number").toString());
         Optional<AutonomousSystem> as = randomizerService.getAutonomousSystem(asNumber);
         return as.isPresent() ? as.get() : Collections.singletonMap("ERROR", "AS " + asNumber + " not found");
+    }
+
+    @Put
+    @Post
+    public Object addPrefixToPool(String json) throws IOException {
+        IRandomizerService randomizerService = (IRandomizerService) getContext().getAttributes().get(IRandomizerService.class.getCanonicalName());
+
+        Integer asNumber = Integer.valueOf(getRequestAttributes().get("as-number").toString());
+        Optional<AutonomousSystem> as = randomizerService.getAutonomousSystem(asNumber);
+        if (!as.isPresent()) {
+            return Collections.singletonMap("ERROR", "AS " + asNumber + " not found");
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode prefixNode = objectMapper.readTree(json).get("prefix");
+        if (prefixNode == null) {
+            return Collections.singletonMap("ERROR", "'prefix' node expected but not found");
+        }
+
+        IPv4AddressWithMask prefix;
+        try {
+           prefix = IPv4AddressWithMask.of(prefixNode.asText());
+        } catch (IllegalArgumentException e) {
+            return Collections.singletonMap("ERROR", e.getMessage());
+        }
+
+        as.get().addPrefix(prefix);
+        return Collections.singletonMap("SUCCESS", "Prefix " + prefix + " added to AS " + asNumber + " prefix pool");
     }
 }

c367c9cf19fb91f79ed060b582ceaf32ac4b5161
Geddings Barrineau <cbarrin@g.clemson.edu>
2018-02-27 11:47:47 -0500

    Adding IPv6 ping test.

--- examples/tests/test_ping_ipv6.py
+++ examples/tests/test_ping_ipv6.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 import os
-
-import pytest as pytest
-from mininet.cli import CLI
-from mininet.log import info
-from mininet.net import Mininet
-from nodes import Floodlight
+import time
 from os import makedirs
 from os import path
+
+from mininet.log import info
+from mininet.net import Mininet
+
+from nodes import Floodlight
 from topologies.lineartopo import LinearTopo
 
 HOME_FOLDER = os.getenv('HOME')
@@ -39,65 +39,64 @@ def stopNetwork():
         info('** Tearing down network\n')
         net.stop()
 
-@pytest.mark.skip(reason="This test is still under construction.")
+
 def test_ping():
     startNetwork()
 
-    # # Start the switches and specify a controller
-    # s1 = net.getNodeByName('s1')
-    # s2 = net.getNodeByName('s2')
-    #
-    # # Start each controller
-    # c1 = net.getNodeByName('c1')
-    # c2 = net.getNodeByName('c2')
-    # for controller in net.controllers:
-    #     controller.start()
-    # s1.start([c1])
-    # s2.start([c2])
-    #
-    # # After switch registered, set protocol to OpenFlow 1.5
-    # s1.cmd('ovs-vsctl set bridge s1 protocols=OpenFlow15')
-    # s2.cmd('ovs-vsctl set bridge s2 protocols=OpenFlow15')
+    # Start the switches and specify a controller
+    s1 = net.getNodeByName('s1')
+    s2 = net.getNodeByName('s2')
+
+    # Start each controller
+    c1 = net.getNodeByName('c1')
+    c2 = net.getNodeByName('c2')
+    for controller in net.controllers:
+        controller.start()
+    s1.start([c1])
+    s2.start([c2])
+
+    # After switch registered, set protocol to OpenFlow 1.5
+    s1.cmd('ovs-vsctl set bridge s1 protocols=OpenFlow15')
+    s2.cmd('ovs-vsctl set bridge s2 protocols=OpenFlow15')
 
     # Send non blocking commands to each host
     h1 = net.getNodeByName('h1')
     h2 = net.getNodeByName('h2')
 
     # Set IPv6 addresses
-    h1.cmd('ifconfig h1-eth0 inet6 add fc00::1/64')
-    h2.cmd('ifconfig h2-eth0 inet6 add fc00::2/64')
-    # h1.setIP('10.0.0.1', prefixLen=24)
-    # h2.setIP('50.0.0.1', prefixLen=24)
+    h1.cmd('ifconfig h1-eth0 inet6 add fc00:1111::1/64')
+    h2.cmd('ifconfig h2-eth0 inet6 add fc00:5555::1/64')
 
-    # time.sleep(10)
-    #
-    # # REST API to configure AS1 controller
-    # c1.addMapping("10.0.0.1", "40.0.0.0/24")
-    # c1.addMapping("50.0.0.1", "80.0.0.0/16")
-    #
-    # # REST API to configure AS2 controller
-    # c2.addMapping("10.0.0.1", "40.0.0.0/24")
-    # c2.addMapping("50.0.0.1", "80.0.0.0/16")
-    #
-    # # End-to-end communication setup as below
-    # h1.cmd('route add -net 50.0.0.0 netmask 255.255.255.0 dev h1-eth0')
-    # h2.cmd('route add -net 10.0.0.0 netmask 255.255.255.0 dev h2-eth0')
-    #
-    # time.sleep(3)
-    #
-    # # Query flow rules on each switch and write to log file
-    # s1.cmd('ovs-ofctl dump-flows s1 -O OpenFlow15 > ' + LOG_PATH + ' s1.log')
-    # s2.cmd('ovs-ofctl dump-flows s2 -O OpenFlow15 > ' + LOG_PATH + ' s2.log')
-    #
-    # info("** Testing network connectivity\n")
-    # # packet_loss = net.ping(net.hosts)
-    # result = h1.cmd('ping -i 1 -c 10 ' + str(h2.IP()))
-    # sent, received = net._parsePing(result)
-    # info('Sent:' + str(sent) + ' Received:' + str(received) + '\n')
+    time.sleep(10)
 
-    CLI(net)
+    # REST API to configure AS1 controller
+    c1.addMapping("10.0.0.1", "40.0.0.0/24")
+    c1.addMapping("50.0.0.1", "80.0.0.0/16")
+    c1.addMapping("fc00:1111::1", "fc00:4444::/64")
+    c1.addMapping("fc00:5555::1", "fc00:8888::/64")
 
-    #
+    # REST API to configure AS2 controller
+    c2.addMapping("10.0.0.1", "40.0.0.0/24")
+    c2.addMapping("50.0.0.1", "80.0.0.0/16")
+    c2.addMapping("fc00:1111::1", "fc00:4444::/64")
+    c2.addMapping("fc00:5555::1", "fc00:8888::/64")
+
+    # End-to-end communication setup as below
+    h1.cmd('route -A inet6 add fc00:5555::/64 dev h1-eth0')
+    h1.cmd('ip -6 neigh add fc00:5555::1 lladdr ' + h2.MAC() + ' dev h1-eth0')
+    h2.cmd('route -A inet6 add fc00:1111::/64 dev h2-eth0')
+    h2.cmd('ip -6 neigh add fc00:1111::1 lladdr ' + h1.MAC() + ' dev h2-eth0')
+
+    time.sleep(3)
+
+    # Query flow rules on each switch and write to log file
+    s1.cmd('ovs-ofctl dump-flows s1 -O OpenFlow15 > ' + LOG_PATH + ' s1.log')
+    s2.cmd('ovs-ofctl dump-flows s2 -O OpenFlow15 > ' + LOG_PATH + ' s2.log')
+
+    info("** Testing network connectivity\n")
+    result = h1.cmd('ping6 -c 10 -I h1-eth0 fc00:5555::1')
+    sent, received = net._parsePing(result)
+    info('Sent:' + str(sent) + ' Received:' + str(received) + '\n')
+
     stopNetwork()
-    #
-    # assert sent - received == 0
+    assert sent - received == 0
